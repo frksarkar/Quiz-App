d252d1f118bedb6fa1e83f3cd497beac3c13bb88
omar faruk sarkar <hazrat.omurfaruk@gmail.com>
2023-05-04 01:34:53 +0600

    add timer and element function

--- script.js
+++ script.js
@@ -7,9 +7,17 @@ const quizBox = document.querySelector('.quiz_box');
 const nextBtn = quizBox.querySelector('.next_btn');
 const scoreBox = document.querySelector('.score_box');
 const exitScoreBoxBtn = scoreBox.querySelector('.quit');
+const optionsList = document.querySelector('.option_list');
+const timeCount = quizBox.querySelector('.timer .timer_sec');
+const xmarkIcon =
+	'<div class="icon cross"><i class="fa-solid fa-xmark"></i></div>';
+const tickIcon =
+	'<div class="icon tick"><i class="fa-sharp fa-solid fa-check"></i></div>';
 const number = {
 	questionCount: 0,
 	score: 0,
+	counter: null,
+	timerValue: 15,
 };
 
 // start quiz button clicked
@@ -31,12 +39,10 @@ continueBtn.addEventListener('click', () => {
 	showQuestions(0);
 });
 
-// let questionCount = 0;
-
 // getting questions and options from array
 function showQuestions(index) {
 	const questionText = document.querySelector('.que_text');
-	const optionsList = document.querySelector('.option_list');
+
 	const questionTag = `<span>${question[index].number}. ${question[index].question} </span>`;
 	questionText.innerHTML = questionTag;
 	const optionsTag = `<div class="option"><span>${question[index].options[0]}</span></div>
@@ -44,11 +50,13 @@ function showQuestions(index) {
     <div class="option"><span>${question[index].options[2]}</span></div>
     <div class="option"><span>${question[index].options[3]}</span></div>`;
 	optionsList.innerHTML = optionsTag;
+	const options = optionsList.querySelectorAll('.option');
 	questionCountNum();
-	const option = optionsList.querySelectorAll('.option');
-	option.forEach((element) => {
+	options.forEach((element) => {
 		element.setAttribute('onclick', 'optionSelected(this)');
 	});
+	questionCountNum();
+	startTimer(number.timerValue);
 }
 
 function questionCountNum() {
@@ -60,21 +68,48 @@ function questionCountNum() {
 }
 
 function optionSelected(answer) {
+	clearInterval(number.counter);
 	const userAnswer = answer.textContent;
-	if (userAnswer === question[number.questionCount].answer) {
+	const correctAnswer = question[number.questionCount].answer;
+	const allChildren = optionsList.children.length;
+	if (userAnswer === correctAnswer) {
 		number.score++;
 		answer.classList.add('correct');
+		answer.insertAdjacentHTML('beforeend', tickIcon);
 	} else {
+		answer.classList.add('inCorrect');
+		answer.insertAdjacentHTML('beforeend', xmarkIcon);
+
+		// if answer is wrong than auto magically select correct answer
+
+		for (let i = 0; i < allChildren; i++) {
+			if (optionsList.children[i].textContent === correctAnswer) {
+				optionsList.children[i].setAttribute('class', 'option correct');
+			}
+		}
 	}
+
+	// for (let i = 0; i < allChildren; i++) {
+	// 	optionsList.children[i].classList.add('disable');
+	// }
+	childElementDisable(allChildren, optionsList);
 }
 
+const childElementDisable = (allChildren, optionsList) => {
+	for (let i = 0; i < allChildren; i++) {
+		optionsList.children[i].classList.add('disable');
+	}
+};
+
 nextBtn.addEventListener('click', () => {
+	clearInterval(number.counter);
 	number.questionCount++;
 	if (number.questionCount === question.length) {
 		quizBox.classList.remove('activeInfo');
 		infoBox.classList.remove('activeInfo');
 		number.questionCount = 0;
 		showScore();
+		clearInterval(counter);
 	} else {
 		showQuestions(number.questionCount);
 		questionCountNum();
@@ -89,3 +124,17 @@ function showScore() {
 exitScoreBoxBtn.addEventListener('click', () => {
 	scoreBox.classList.remove('activeInfo');
 });
+
+function startTimer(time) {
+	number.counter = setInterval(() => {
+		timer();
+	}, 1000);
+
+	function timer() {
+		timeCount.innerHTML = time;
+		time--;
+		if (time === -1) {
+			clearInterval(number.counter);
+		}
+	}
+}
